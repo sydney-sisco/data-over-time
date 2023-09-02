2fa2a9444b5d07987f290c21aed0b1fa8d35e625
Sydney Sisco <hello@sydsis.co>
2023-09-02 15:21:17 -0700

    delete more stuff we don't need

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,10 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import { ApiTest } from './components/ApiTest'
-import { ConnectionState } from './components/ConnectionState'
 import futureLogo from '/future.svg'
 import './App.css'
 import Record from './components/Record'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
@@ -15,18 +13,7 @@ function App() {
           <img src={futureLogo} className="logo" alt="future logo" />
         </a>
       </div>
-      <h1>webapp-template</h1>
-      <p className="read-the-docs">
-        Click on the logo to learn more
-      </p>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
+      <h1>Data over Time</h1>
       <Record />
       <ApiTest endpoint="/api/test" />
       {/* <ApiTest endpoint="/api/test_not_found" /> */}
