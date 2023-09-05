28cc95f0d42df788bf0e66f60973ccde49f44f7b
Sydney Sisco <hello@sydsis.co>
2023-09-05 10:42:08 -0700

    store null values if something is missing

--- backend/features/record.js
+++ backend/features/record.js
@@ -3,30 +3,19 @@ const firestore = require('../utils/firestore')
 // save to a user's collection
 const saveData = async (dataObj, userId) => {
 
-  const name = dataObj.name || null;
-  // name = caffeine
-
-  // TODO: data can be an array of objects
-  const data = dataObj.data || null;
-  // {
-  //  "units": "mg",
-  //  "value": 160,
-  //  "type": "rockstar pure zero punch",
-  // }
-
-  // generates a UTC timestamp in the format of YYYY-MM-DDTHH:MM:SSZ
-  const createdAt = new Date().toISOString();
+  const category = dataObj.category || null;
+  const values = dataObj.values || null;
+  const createdAt = new Date().toISOString(); // YYYY-MM-DDTHH:MM:SSZ
 
   try {
-    // const { text, deviceId } = thoughtData; // thought destructure here
     const deviceId = null;
     const dataCollectionRef = firestore
       .collection('users')
       .doc(userId)
       .collection('data');
     const docRef = await dataCollectionRef.add({
-      data,
-      name,
+      values,
+      category,
       deviceId,
       createdAt: createdAt,
       updatedAt: createdAt,
